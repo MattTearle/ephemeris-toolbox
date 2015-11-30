e81d98ed994e85c3183d33c970aa61cc21006c92
unknown <mtearle@AH-MTEARLE.dhcp.mathworks.com>
2015-11-29 21:31:11 -0500

    reordered planets and times in dimensions of 3-D array

--- heliocentricposition.m
+++ heliocentricposition.m
@@ -1,38 +1,19 @@
 function x = heliocentricposition(orbelements)
 
+[numplanet,~,numtime] = size(orbelements);
+
 % a = semimajor;
 % e = eccentricity;
 % I = inclination;
 % L = meanlong;
 % w = perihelion;
 % Q = ascendingnode;
-
-[numtime,~,numplanet] = size(orbelements);
-
-a = reshape(orbelements(:,1,:),numtime,numplanet);
-e = reshape(orbelements(:,2,:),numtime,numplanet);
-I = reshape(orbelements(:,3,:),numtime,numplanet);
-L = reshape(orbelements(:,4,:),numtime,numplanet);
-w = reshape(orbelements(:,5,:),numtime,numplanet);
-Q = reshape(orbelements(:,6,:),numtime,numplanet);
-
-% a = orbelements(:,1,:);
-% e = orbelements(:,2,:);
-% I = orbelements(:,3,:);
-% L = orbelements(:,4,:);
-% w = orbelements(:,5,:);
-% Q = orbelements(:,6,:);
-
-% if nargin==1
-%         e = a.eccentricity;
-%         I = a.inclination;
-%         L = a.meanlong;
-%         w = a.perihelion;
-%         Q = a.ascendingnode;
-%         a = a.semimajor;
-% elseif nargin~=6
-%     error('Wrong number of inputs')
-% end
+a = reshape(orbelements(:,1,:),numplanet,numtime);
+e = reshape(orbelements(:,2,:),numplanet,numtime);
+I = reshape(orbelements(:,3,:),numplanet,numtime);
+L = reshape(orbelements(:,4,:),numplanet,numtime);
+w = reshape(orbelements(:,5,:),numplanet,numtime);
+Q = reshape(orbelements(:,6,:),numplanet,numtime);
 
 anom = pi*mod(L - w,360)/180;
 Kep = @(E) E - e.*sin(E) - anom;
@@ -43,11 +24,10 @@ while (max(abs(dE(:))) > 1e-6)
     E = E - dE./dKep(E);
     dE = Kep(E);
 end
-% E = fzero(Kep,anom);
 
-x = zeros(numtime,3,numplanet);
-for k = 1:numplanet
-    for j = 1:numtime
+x = zeros(numplanet,3,numtime);
+for k = 1:numtime
+    for j = 1:numplanet
         
         xprime = [a(j,k)*(cos(E(j,k))-e(j,k));...
             a(j,k)*sqrt(1-e(j,k)^2)*sin(E(j,k))];
