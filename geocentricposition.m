76e1d7ce46f50f9fc0227606baf55acf311e4bfd
mtearle <mtearle@AH-MTEARLE.dhcp.mathworks.com>
2015-12-04 15:32:12 -0500

    reconfigured code to have a single geocentricposition function with local functions (don't really need to expose utility functions to users)

--- geocentricposition.m
+++ geocentricposition.m
@@ -1,29 +1,20 @@
-function planet = getorbitalelements(t,pnames)
+function xgeo = geocentricposition(t,planets)
 
+% Select planets from official list. Add Earth to planet list, for later
+% reference (heliocentric -> geocentric)
 allnames = {'Sun';'Mercury';'Venus';'Mars';'Jupiter';'Saturn';...
     'Uranus';'Neptune';'Pluto';'Earth'};
-if nargin<2
-    pnames = allnames;
-    if nargin<1
-        t = datetime('now');
-    end
-elseif ischar(pnames) && size(pnames,1)==1
-    pnames = {pnames};
-end
-if ~iscellstr(pnames)
-    error('Ephemeris:OrbitalElements:PlanetNameInput',...
-        'Planet names must be given as a string or cell array of strings')
-end
-t = datetime(t);
+[~,~,idx] = intersect(lower([planets;{'Earth'}]),lower(allnames),'stable');
 
-[~,~,idx] = intersect(lower(pnames),lower(allnames),'stable');
-numplanet = length(idx);
-if (numplanet < length(pnames)) || (length(pnames) > length(allnames))
-    error('Ephemeris:OrbitalElements:PlanetNames',...
-        ['Invalid planet name.',char(10),'Allowable planets are: ',...
-        sprintf('%s, ',allnames{1:end-1}),allnames{end},'.'])
-end
+% Get orbital element information and calculate heliocentric position of
+% all chosen planets, plus Earth (for reference)
+oe = orbitalelements(idx,t);
+xhelio = heliocentricposition(oe);
+% Calculate cartesian position relative to Earth
+xgeo = bsxfun(@minus,xhelio(1:end-1,:,:),xhelio(end,:,:));
 
+
+function planet = orbitalelements(idx,t)
 OEAll = ...
 [0 0 0 0 0 0;
  0.38709927      0.20563593      7.00497902      252.25032350     77.45779628     48.33076593
@@ -52,9 +43,54 @@ OE = OEAll(idx,:);
 Delta = DeltaAll(idx,:);
 dt = j2000(t(:))/36525;
 
+numplanet = length(idx);
 planet = zeros(length(dt),6,numplanet);
 for k = 1:numplanet
     planet(:,:,k) = bsxfun(@plus,OE(k,:),bsxfun(@times,dt,Delta(k,:)));
 end
 
 planet = permute(planet,[3 2 1]);
+
+function x = heliocentricposition(orbelements)
+[numplanet,~,numtime] = size(orbelements);
+
+% a = semimajor;
+% e = eccentricity;
+% I = inclination;
+% L = meanlong;
+% w = perihelion;
+% Q = ascendingnode;
+a = reshape(orbelements(:,1,:),numplanet,numtime);
+e = reshape(orbelements(:,2,:),numplanet,numtime);
+I = reshape(orbelements(:,3,:),numplanet,numtime);
+L = reshape(orbelements(:,4,:),numplanet,numtime);
+w = reshape(orbelements(:,5,:),numplanet,numtime);
+Q = reshape(orbelements(:,6,:),numplanet,numtime);
+
+anom = pi*mod(L - w,360)/180;
+Kep = @(E) E - e.*sin(E) - anom;
+dKep = @(E) 1 - e.*cos(E);
+E = anom;
+dE = Kep(E);
+while (max(abs(dE(:))) > 1e-6)
+    E = E - dE./dKep(E);
+    dE = Kep(E);
+end
+
+x = zeros(numplanet,3,numtime);
+for k = 1:numtime
+    for j = 1:numplanet
+        xprime = [a(j,k)*(cos(E(j,k))-e(j,k));...
+            a(j,k)*sqrt(1-e(j,k)^2)*sin(E(j,k))];
+        
+        sQ = sind(Q(j,k));
+        cQ = cosd(Q(j,k));
+        cw = cosd(w(j,k)-Q(j,k));
+        sw = sind(w(j,k)-Q(j,k));
+        cI = cosd(I(j,k));
+        sI = sind(I(j,k));
+        x(j,:,k) = [cw*cQ-sw*sQ*cI,-sw*cQ-cw*sQ*cI;...
+            cw*sQ+sw*cQ*cI,-sw*sQ+cw*cQ*cI;sw*sI,cw*sI]*xprime;
+    end
+end
+
